2a1a4de5bea157fce2ae0f116ef42ee58d2b8e5a
Michiel de Jong (build server) <michiel+build@unhosted.org>
2020-10-26 15:56:24 +0000

    more update-tree.sh improvements

--- scripts/update-tree.sh
+++ scripts/update-tree.sh
@@ -13,7 +13,8 @@ function updateRepo1 {
   echo Updating $1
   cd workspaces/$1
   sed -i -e 's/https...github.com./git@github.com:/g' .git/config
-  git reset master
+  git checkout master
+  git reset origin/master
   git checkout -- .
   git pull
 }
@@ -27,6 +28,7 @@ function updateRepo3 {
   npm test
   npm run build
   ( git commit -am"Update dependencies" && npm version patch && npm publish -timeout=9999999 ) || echo No change in $1
+  git push origin master --follow-tags
   cd ../..
   echo Finished $1
   # updateRepo $3 ""
@@ -38,8 +40,8 @@ function updateRepo {
   updateRepo3 $1
 }
 
-# updateRepo jose # @solid/jose (@sinonjs/text-encoding, base64url, isomorphic-webcrypto)
-# updateRepo oidc-rp # @solid/oidc-rp (@solid/jose, base64url, isomorphic-webcrypto, node-fetch, standard-http-error, whatwg-url)
+updateRepo jose # @solid/jose (@sinonjs/text-encoding, base64url, isomorphic-webcrypto)
+updateRepo oidc-rp # @solid/oidc-rp (@solid/jose, base64url, isomorphic-webcrypto, node-fetch, standard-http-error, whatwg-url)
 
 updateRepo1 solid-auth-client # solid-auth-client (@babel/runtime, auth-header, commander, isomorphic-fetch, @solid/oidc-rp)
 # updateRepo2 solid-auth-client
@@ -50,24 +52,38 @@ updateRepo pane-registry # pane-registry (rdflib)
 updateRepo rdflib # rdflib (jsonld, n3, xmldom)
 updateRepo solid-namespace # solid-namespace
 
-updateRepo1 solid-ui # solid-ui (pane-registry, rdflib, solid-auth-client, solid-namespace)
-updateRepo2 solid-ui
-npm install --save-dev @typescript-eslint/eslint-plugin@3
-npm install --save-dev @typescript-eslint/parser@3
-updateRepo3 solid-ui
+updateRepo solid-ui # solid-ui (pane-registry, rdflib, solid-auth-client, solid-namespace)
 
-updateRepo activitystreams-pane
-updateRepo chat-pane # chat-pane (solid-ui)
+updateRepo1 activitystreams-pane
+updateRepo2 activitystreams-pane
+npm install --save-dev webpack@4
+npm install --save-dev webpack-cli@3
+updateRepo3 activitystreams-pane
+
+updateRepo1 chat-pane # chat-pane (solid-ui)
+updateRepo2 chat-pane # chat-pane (solid-ui)
+npm install --save-dev webpack@4
+npm install --save-dev webpack-cli@3
+updateRepo3 chat-pane # chat-pane (solid-ui)
+
 updateRepo contacts-pane # contacts-pane (solid-ui)
 updateRepo folder-pane # folder-pane (solid-ui)
 updateRepo issue-pane # issue-pane (solid-ui, pane-registry, rdflib)
-updateRepo meeting-pane # meeting-pane (solid-ui)
+
+updateRepo1 meeting-pane # meeting-pane (solid-ui)
+updateRepo2 meeting-pane # meeting-pane (solid-ui)
+npm install --save-dev webpack@4
+npm install --save-dev webpack-cli@3
+updateRepo3 meeting-pane # meeting-pane (solid-ui)
+
 updateRepo source-pane # source-pane (solid-ui)
 
 updateRepo1 solid-panes # solid-panes (chat-pane, contacts-pane, folder-pane, issue-pane, meeting-pane, pane-registry, rdflib, solid-ui, source-pane)
 updateRepo2 solid-panes
 npm install --save-dev @typescript-eslint/eslint-plugin@3
 npm install --save-dev @typescript-eslint/parser@3
+npm install --save-dev webpack@4
+npm install --save-dev webpack-cli@3
 updateRepo3 solid-panes
 
 updateRepo1 mashlib # mashlib (rdflib, solid-panes, solid-ui)
@@ -84,8 +100,11 @@ updateRepo keychain # @solid/keychain
 updateRepo oidc-op # @solid/oidc-op (@solid/keychain)
 updateRepo solid-multi-rp-client # solid-multi-rp-client (oidc-rp)
 updateRepo oidc-auth-manager # oidc-auth-manager (oidc-rp, oidc-op, solid-multi-rp-client)
-updateRepo solid-auth-oidc @solid/solid-auth-oidc (@solid/oidc-rp)
-updateRepo node-solid-server (@solid/acl-check, @solid/oidc-auth-manager, rdflib, mashlib, solid-auth-client, solid-namespace, solid-ws, @solid/oidc-op, @solid/solid-auth-oidc)
+updateRepo solid-auth-oidc # @solid/solid-auth-oidc (@solid/oidc-rp)
+updateRepo1 node-solid-server # solid-server (@solid/acl-check, @solid/oidc-auth-manager, rdflib, mashlib, solid-auth-client, solid-namespace, solid-ws, @solid/oidc-op, @solid/solid-auth-oidc)
+updateRepo2 node-solid-server # solid-server (@solid/acl-check, @solid/oidc-auth-manager, rdflib, mashlib, solid-auth-client, solid-namespace, solid-ws, @solid/oidc-op, @solid/solid-auth-oidc)
+npm install bootstrap@3
+updateRepo3 node-solid-server # solid-server (@solid/acl-check, @solid/oidc-auth-manager, rdflib, mashlib, solid-auth-client, solid-namespace, solid-ws, @solid/oidc-op, @solid/solid-auth-oidc)
 
 updateRepo solid-rest # solid-rest (concat-stream, cross-fetch, fs-extra, mime-types)
 updateRepo solid-cli # solid-cli (@solid/oidc-rp)
