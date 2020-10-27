56b60d24e5b6ade6999820021271929dd7869546
Michiel de Jong <michiel@unhosted.org>
2020-10-27 09:08:46 +0100

    This code was moved to solid/solidos

--- scripts/update-mashlib-repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git checkout master
-git pull
-npm install rdflib@latest solid-namespace@latest solid-ui@latest solid-panes@latest
-git commit -am'npm install rdflib@latest solid-namespace@latest solid-ui@latest solid-panes@latest'
-npm version minor
-npm publish -timeout=9999999
--- scripts/update-this-panes-repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git checkout master
-git pull
-npm install solid-ui@latest
-git commit -am'npm install solid-ui@latest'
-npm version minor
-npm publish -timeout=9999999
